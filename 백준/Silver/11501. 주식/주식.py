6c81f186cd34f4f9badf7d452d1f2ad111f0e4a3
chkim417 <51479663+chkim417@users.noreply.github.com>
2023-03-19 01:33:47 +0900

    [Silver II] Title: 주식, Time: 5620 ms, Memory: 394796 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,24 +1,30 @@
-def stock(stock_list, start, end):
-    sum = 0
-    while start < end:
-        point_value = 0
-        point = -1
-        for i in range(end, start-1, -1):
-            if i and stock_list[i] < stock_list[i-1]:
-                end -= 1
-            else:
-                break
-        for i in range(end, start-1, -1):
-            if point_value < stock_list[i]:
-                point_value = stock_list[i]
-                point = i
-        for i in range(start, point):
-            sum += point_value - stock_list[i]
-        start = point + 1
-    return sum
+t = int(input())
+
+for _ in range(t):
+    stock_count= int(input())
+    stock_list=list(map(int,input().split()))
+    action=[-1 for _ in range(stock_count)] # -1:null 0: buy 1: sell
+    marked=-1
+    sum=0
+    points=[]
+    for i in range(1,stock_count-1):
+        if stock_list[i-1]<stock_list[i] and stock_list[i]>stock_list[i+1]:
+            points.append([i,stock_list[i]])
+    points.append([0,stock_list[0]])
+    points.append([stock_count-1,stock_list[-1]])
+    points.sort(key=lambda x:x[1], reverse=True)
+    for point in points:
+        if marked>point[0]:
+            continue
+        else:
+            for idx in range(marked+1, point[0]):
+                if point[1]> stock_list[idx]:
+                    sum+= point[1]-stock_list[idx]
+                    marked=point[0]
+    print(sum)
 
 
-for _ in range(int(input())):
-    n = int(input())
-    stock_list = list(map(int,input().split()))
-    print(stock(stock_list, 0, n-1))
+
+
+
+
